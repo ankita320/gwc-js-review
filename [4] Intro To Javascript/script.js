773fe22feb4a0f1899a02b698e5228dad476702e
Ankita Saha <76600603+ankita320@users.noreply.github.com>
2022-01-24 14:27:52 -0500

    Create script.js

---
+++ [4] Intro To Javascript/script.js
@@ -0,0 +1,56 @@
+// Try it out #2 👩‍💻👩‍💻
+// ex: alert("boop")
+
+// Make an alert for what you had for lunch today!
+// YOUR CODE HERE 💥💥💥
+alert("mcdonalds")
+//===========================================
+
+// more on variables: 
+// Instantiating variables
+var year
+var month = "November"
+var date = 12
+// assigning values to variables
+year = 2021
+// console.logging it out 
+// remember: we can also console.log variables!
+// console.log(year)
+
+// ================================================
+
+// Try it out #3 👩‍💻👩‍💻
+// Create variables for your name, your age, and the number of pets you have 
+// Calculate the sum of the number of pets you have and your age
+// Then console.log() everything!
+// ex:
+// var name = "Samantha"
+// var age = 16
+// var numOfPets = 1
+// he's a really old dog who doesn't care about anything <3
+// var sum = age + numOfPets 
+
+// console.log(name)
+// console.log(age)
+// console.log(numOfPets)
+// console.log(sum)
+
+//Your turn! 💥💥💥
+var names = "Ankita"
+var ages = 14
+var numOfPet = 0
+var sums = ages + numOfPet
+
+console.log(names)
+console.log(ages)
+console.log(numOfPet)
+console.log(sums)
+// ================================================
+
+// Try it out #4 👩‍💻👩‍💻
+// Check out the html code for the header!
+
+// Your turn! 💥💥💥
+// Make it so that when you press the header, it console.log's your birthday
+
+// ================================================
